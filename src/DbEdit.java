81dc7b80ec715e5af2a58abdb5642e5c4487a0e0
Mateusz <Mateusz@192.168.1.8>
2019-05-05 22:05:51 +0200

    Poprawione formatowanie i czyszczenie kodu. Brak zmian w funkcjonalności.

--- src/DbEdit.java
+++ src/DbEdit.java
@@ -1,35 +1,41 @@
 import java.sql.*;
 
-
-
 public class DbEdit {
-	
-String query;
 
-	public void enterData (String author, String title, int items) {
-	try {   	
-      	//tworzenie obiektu Connection, który jest niezbêdny do komunikacji z baz¹ SQL - kierujemy go do bazy/schema, dodatkowo z inf o kodowaniu znaków
-          Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test_schema?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "mateusz", "teleg2");
-          
-          //tworzenie obiektu Statement, który musi zostaæ przekazany przez obiekt Connection do bazy, by wykonaæ zapytanie   
-          Statement statement = con.createStatement();
+	String query;
 
-          //tworzenie w³aœciwego zapytania sql
-          //INSERT INTO Heroes (id, name, siblings) VALUES (2, 'Ron Weasley', 6);  
-        query=("INSERT INTO books (author, title, items) VALUES ('" + author + "','" + title + "',"+ items +")");
-          
-          //wywo³ywanie metody executeUpdate z parametrem query -> do wstawiania nowych danych do tabeli
+	public void enterData(String author, String title, int items) {
+		try {
+			// tworzenie obiektu Connection, który jest niezbêdny do komunikacji z baz¹ SQL
+			// - kierujemy go do bazy/schema, dodatkowo z inf o kodowaniu znaków
+			Connection con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/test_schema?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+					"mateusz", "teleg2");
+
+			// tworzenie obiektu Statement, który musi zostaæ przekazany przez obiekt
+			// Connection do bazy, by wykonaæ zapytanie
+			Statement statement = con.createStatement();
+
+			// tworzenie w³aœciwego zapytania sql
+			// INSERT INTO Heroes (id, name, siblings) VALUES (2, 'Ron Weasley', 6);
+			query = ("INSERT INTO books (author, title, items) VALUES ('" + author + "','" + title + "'," + items
+					+ ")");
+
+			// wywo³ywanie metody executeUpdate z parametrem query -> do wstawiania nowych
+			// danych do tabeli
 			statement.executeUpdate(query);
-			
-			//tworzenie drugiego zapytania do bazy, tym razem wpisuj¹c jego treœæ bezposednio do parametru metody
-			//obiekt ResultSet s³u¿y do oczytywania danych z bazy - do ustalenia szczegó³y
-         /* ResultSet rs2 = statement.executeQuery("select * from test");
-          while (rs2.next())
-              System.out.println(rs2.getInt(1) + "  ");*/
-          con.close();
-      } catch (Exception e) {
-          System.err.println(e);
-      }
-      
-    }
+
+			// tworzenie drugiego zapytania do bazy, tym razem wpisuj¹c jego treœæ
+			// bezposednio do parametru metody
+			// obiekt ResultSet s³u¿y do oczytywania danych z bazy - do ustalenia szczegó³y
+			/*
+			 * ResultSet rs2 = statement.executeQuery("select * from test"); while
+			 * (rs2.next()) System.out.println(rs2.getInt(1) + "  ");
+			 */
+			con.close();
+		} catch (Exception e) {
+			System.err.println(e);
+		}
+
+	}
 }
--- src/Okno.java
+++ src/Okno.java
@@ -1,5 +1,4 @@
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JTextPane;
 import java.awt.BorderLayout;
@@ -9,15 +8,9 @@ import javax.swing.JTabbedPane;
 import javax.swing.JPanel;
 import javax.swing.BoxLayout;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import java.awt.event.ActionEvent;
 import javax.swing.JTable;
-import java.awt.GridLayout;
 import javax.swing.JSpinner;
-import java.awt.FlowLayout;
 import java.awt.Component;
 import javax.swing.SwingConstants;
 
@@ -53,64 +46,56 @@ public class Okno {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		DbEdit baseEditor=new DbEdit();
-		
+		DbEdit baseEditor = new DbEdit();
+
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		frame.getContentPane().add(tabbedPane, BorderLayout.CENTER);
-		
+
 		JPanel panel1 = new JPanel();
 		tabbedPane.addTab("Dodawanie", null, panel1, null);
 		panel1.setLayout(new BoxLayout(panel1, BoxLayout.X_AXIS));
-		
+
 		JLabel lblNewLabel = new JLabel("Autor");
 		lblNewLabel.setVerticalAlignment(SwingConstants.TOP);
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 		panel1.add(lblNewLabel);
-		
+
 		JTextPane authorData = new JTextPane();
 		panel1.add(authorData);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Tytu\u0142");
 		panel1.add(lblNewLabel_1);
-		
+
 		JTextPane titleData = new JTextPane();
 		panel1.add(titleData);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("Egzemplarze");
 		panel1.add(lblNewLabel_2);
-		
-		
-		
-		
+
 		JSpinner spinnerItems = new JSpinner();
 		panel1.add(spinnerItems);
-		
-		
-		
-	
+
 		JButton btnDodajDoBazy = new JButton("Dodaj do bazy");
 		btnDodajDoBazy.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-			baseEditor.enterData(authorData.getText(), titleData.getText(), (Integer)spinnerItems.getValue());
-				
-				 
-				
+				baseEditor.enterData(authorData.getText(), titleData.getText(), (Integer) spinnerItems.getValue());
+
 			}
 		});
-		
+
 		panel1.add(btnDodajDoBazy);
 		JPanel panel2 = new JPanel();
 		tabbedPane.addTab("Wypo\u017Cyczanie", null, panel2, null);
 		panel2.setLayout(new BoxLayout(panel2, BoxLayout.X_AXIS));
-		
+
 		JLabel lblResult = new JLabel("New label");
 		panel2.add(lblResult);
-		
+
 		table = new JTable();
 		panel2.add(table);
 	}
